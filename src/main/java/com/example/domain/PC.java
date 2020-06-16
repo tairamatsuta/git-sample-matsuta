e24265d01b886fff493018f91e5b8fa3aa2e1e22
tairamatsuta <taira.matsuta@rakus.co.jp>
2020-06-16 12:23:38 +0900

    PCドメインの実装

---
+++ src/main/java/com/example/domain/PC.java
@@ -0,0 +1,20 @@
+package com.example.domain;
+
+public class PC {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "PC [id=" + id + "]";
+	}
+	
+}
