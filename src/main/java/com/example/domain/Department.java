ea37043a356598879aec414607562eb7bf57fee6
tairamatsuta <taira.matsuta@rakus.co.jp>
2020-06-16 11:57:08 +0900

    second commit

---
+++ src/main/java/com/example/domain/Department.java
@@ -0,0 +1,20 @@
+package com.example.domain;
+
+public class Department {
+
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + "]";
+	}
+	
+}
